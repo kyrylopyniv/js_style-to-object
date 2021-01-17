12ba795bd816d88587c49a632a027c0adae63aa5
Kyrylo Pyniv <pynivkm@gmail.com>
2021-01-17 02:27:37 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,25 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const objKeys = [];
+  const objValues = [];
+  const splittedSource = sourceString.split(';');
+  const styleObj = {};
+
+  splittedSource.forEach(element => {
+    for (const i in element) {
+      if (element[i] === ':') {
+        objKeys.push(element.slice(0, i).trim());
+        objValues.push(element.slice(parseInt(i) + 1).trim());
+      }
+    }
+  });
+
+  for (const el in objKeys) {
+    styleObj[objKeys[el]] = `${objValues[el]}`;
+  }
+
+  return styleObj;
 }
 
 module.exports = convertToObject;
